cd08b15377740eb622e0488a9e85409ee1a95636
Shadia Ramsahye <shadiaramsahye@live.com>
2021-03-19 18:02:17 +0100

    last minute panic push

--- api/app.py
+++ api/app.py
@@ -1,8 +1,23 @@
 
+import pandas as pd
+import joblib
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+# i have no idea what i am doing
+
+model = model.joblib
 
 # define a root `/` endpoint
 @app.get("/")
